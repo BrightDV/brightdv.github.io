e288b9fbae8c5c5e46197cb00529b90bf190ee40
BrightDV <92821484+BrightDV@users.noreply.github.com>
2023-06-12 17:16:25 +0200

    upload final version with manifset

--- boxbox/flutter_service_worker.js
+++ boxbox/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "manifest.json": "f1af40b347fff6ad9a4f4175997c5b00",
+  "manifest.json": "7294a35033fe4a97e336d6c330982337",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -32,13 +32,13 @@ const RESOURCES = {
 "assets/FontManifest.json": "edaeb9cede3cf5e19864954a8e50c30c",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "version.json": "527beccf0c3f92bf39bc8b32f18c5c17",
-"index.html": "63d85be75bc3001f13ca5213d8945607",
-"/": "63d85be75bc3001f13ca5213d8945607",
+"index.html": "e7311e502d445607a474d3fad59eed59",
+"/": "e7311e502d445607a474d3fad59eed59",
 "favicon.png": "31354e2ea9d10011172c65088525f929",
 "icons/ic_launcher.png": "33e759627ba1b546bb921356170f87b5",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"main.dart.js": "cc2458d6b7b91f80bbb5d1d8d1b7b456"
+"icons/Icon-192.png": "33e759627ba1b546bb921356170f87b5",
+"icons/Icon-512.png": "f034c476e9c70c91dbfa4904f1ecb65f",
+"main.dart.js": "8904aa2696f81f496ff43e3b8b7cfafe"
 };
 
 // The application shell files that are downloaded before a service worker can
